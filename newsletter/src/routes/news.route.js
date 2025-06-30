846c604149a424b092a5f4e48f4064a34566586a
Marcelin TINGOUGOUI <marcelintingougoui3@gmail.com>
2025-06-30 12:38:50 +0200

    Correction des routes API pour utiliser des tirets au lieu de soulignés et mise à jour de la dépendance dotenv à la version 16.6.1

--- newsletter/src/routes/news.route.js
+++ newsletter/src/routes/news.route.js
@@ -3,7 +3,7 @@ const { getNews, addNews } = require("../controllers/news.controller");
 const express = require("express");
 const router = express.Router();
 
-router.get("/get_news", getNews);
-router.post("/add_news", addNews);
+router.get("/get-news", getNews);
+router.post("/add-news", addNews);
 
 module.exports = router;
--- proxy/server.js
+++ proxy/server.js
@@ -9,12 +9,12 @@ app.use(require("express").urlencoded({ extended: true }));
 const proxyProduct = createProxyMiddleware({
   target: process.env.PRODUCT_SERVICE_URL || "http://localhost:3030",
   changeOrigin: true,
-  pathRewrite: path => path,
+  // pathRewrite: path => path,
  
-  // pathRewrite: {
-  //   "^/api/product/get_all_product": "/api/product/get_all_product",
-  //   "^/api/product/add_product": "/api/product/add_product",
-  // },
+  pathRewrite: {
+    "^/api/product/get-all-products": "/api/product/get-all-products",
+    "^/api/product/add-product": "/api/product/add-product",
+  },
   onProxyReq: (proxyReq, req, res) => {
     // You can modify the request here if needed
     console.log(`Proxying request: ${req.method} ${req.url}`);
@@ -56,13 +56,13 @@ const proxyPanier = createProxyMiddleware({
   },
 });
 
-app.get("/api/product/get_all_product", proxyProduct);
-app.post("/api/product/add_product", proxyProduct);
+app.use("api/product/get-all-products", proxyProduct);
+app.use("/api/product/add-product", proxyProduct);
 
-app.post("/api/news/add_news", proxyNewsLetter);
-app.get("/api/news/get_news", proxyNewsLetter);
+app.use("/api/news/add-news", proxyNewsLetter);
+app.use("/api/news/get-news", proxyNewsLetter);
 
-app.post("/api/panier/add", proxyPanier);
+app.("/api/panier/add", proxyPanier);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
